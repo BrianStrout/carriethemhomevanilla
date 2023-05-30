70f3fa25bcb903f8fe5f27d1ccfe1f274a278b5a
Brian J. Strout <brianjstrout@gmail.com>
2023-05-30 19:49:49 -0400

    js media inquiry

--- src/modules/mobileMenu.js
+++ src/modules/mobileMenu.js
@@ -3,35 +3,37 @@ let menuOut = false;
 let navMenu = Array.from(document.querySelectorAll(".nav--link"));
 let navHero = document.getElementById("navhero");
 let title = document.querySelector(".title--container");
+let windowDim = window.matchMedia("(max-width: 768px)");
 
 const mobileMenu = () => {
   console.log("!");
-  if (!menuOut) {
-    navMenu.forEach((link) => {
-      link.classList.add("slide--out-menu");
-    });
-    title.classList.add("trans--mobile");
+  if (windowDim.matches) {
+    if (!menuOut) {
+      navMenu.forEach((link) => {
+        link.classList.add("slide--out-menu");
+      });
+      title.classList.add("trans--mobile");
 
-    navHero.classList.add("slide--backinline");
-    menuOut = true;
-    // clearCards();
+      navHero.classList.add("slide--backinline");
+      menuOut = true;
+      // clearCards();
 
-    document.querySelectorAll(".sec--card").forEach((link) => {
-      if (!link.classList.contains("transition--cover"))
-        link.classList.add("transition--cover");
-      link.classList.add("hiiiit");
-    });
+      document.querySelectorAll(".sec--card").forEach((link) => {
+        if (!link.classList.contains("transition--cover"))
+          link.classList.add("transition--cover");
+      });
 
-    return;
-  }
-  if (menuOut) {
-    navMenu.forEach((link) => {
-      link.classList.remove("slide--out-menu");
-    });
-    title.classList.remove("trans--mobile");
-    navHero.classList.remove("slide--backinline");
-    menuOut = false;
-    return;
+      return;
+    }
+    if (menuOut) {
+      navMenu.forEach((link) => {
+        link.classList.remove("slide--out-menu");
+      });
+      title.classList.remove("trans--mobile");
+      navHero.classList.remove("slide--backinline");
+      menuOut = false;
+      return;
+    }
   }
 };
 
